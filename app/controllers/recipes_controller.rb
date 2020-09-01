8a5221b750815a6292905a13d94398923afcad37
Mehreen Tahir <mehreentahir18@gmail.com>
2020-09-01 23:08:21 +0300

    Recipes: Added basic recipe listing page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,7 @@
 class RecipesController < ApplicationController
   def index
+    response = Contentful::RecipeService.new.perform(:recipes_list)
+    @recipes = response[:data]
   end
 
   def show
